a610da9ec6e1927bccee64d2bb90c7401d95d77a
Emory Merryman <emory.merryman@gmail.com>
2017-04-15 20:37:38 +0000

    3 keys

--- create-dot-ssh-volume.sh
+++ create-dot-ssh-volume.sh
@@ -10,8 +10,13 @@ do
         ;;
         --github-user-id)
             GITHUB_USER_ID=${2} &&
-            shift &&
-            shift
+                shift &&
+                shift
+        ;;
+        --report-passphrase)
+            REPORT_PASSPHRASE=${2} &&
+                shift &&
+                shift
         ;;
         *)
             echo Unknown Option: ${1} &&
@@ -21,7 +26,8 @@ do
 done &&
     ([ ! -z "${GITHUB_ACCESS_TOKEN}" ] || (echo GITHUB_ACCESS_TOKEN is not defined. && exit 65)) &&
     ([ ! -z "${GITHUB_USER_ID}" ] || (echo GITHUB_USER_ID is not defined. && exit 66)) &&
-    ([ -z "$(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.dot-ssh)" ] || (echo "There is already a dot-ssh volume." && exit 67)) &&
+    ([ ! -z "${REPORT_PASSPHRASE}" ] || (echo REPORT_PASSPHRASE is not defined. && exit 67)) &&
+    ([ -z "$(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.dot-ssh)" ] || (echo "There is already a dot-ssh volume." && exit 68)) &&
     docker volume create --label com.emorymerryman.tstamp=$(date +%s) --label com.emorymerryman.thirdplanet.structure.dot-ssh &&
     docker \
         run \
@@ -41,18 +47,75 @@ done &&
         --workdir /root/.ssh \
         tidyrailroad/openssh-client:0.0.0 \
         -f /root/.ssh/upstream_id_rsa -P "" -C "upstream" &&
-    KEY=$(docker \
+    (cat <<EOF
+{
+    "title": "upstream",
+    "key": "$(docker \
         run \
         --interactive \
         --rm \
         --volume $(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.dot-ssh):/root/.ssh:ro \
         --workdir /root/.ssh \
         alpine:3.4 \
-        cat upstream_id_rsa.pub) &&
+        cat upstream_id_rsa.pub)"
+}
+EOF
+    ) | docker \
+        run \
+        --interactive \
+        --rm \
+        --volume $(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.dot-ssh):/root/.ssh \
+        tidyrailroad/curl:0.0.0 \
+        --header "Content-Type: application/x-www-form-urlencoded" --user "${GITHUB_USER_ID}:${GITHUB_ACCESS_TOKEN}" --data @- "https://api.github.com/user/keys" &&
+    docker \
+        run \
+        --interactive \
+        --rm \
+        --entrypoint ssh-keygen \
+        --volume $(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.dot-ssh):/root/.ssh \
+        --workdir /root/.ssh \
+        tidyrailroad/openssh-client:0.0.0 \
+        -f /root/.ssh/origin_id_rsa -P "" -C "origin" &&
     (cat <<EOF
 {
-    "title": "upstream",
-    "key": "${KEY}"
+    "title": "origin",
+    "key": "$(docker \
+        run \
+        --interactive \
+        --rm \
+        --volume $(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.dot-ssh):/root/.ssh:ro \
+        --workdir /root/.ssh \
+        alpine:3.4 \
+        cat origin_id_rsa.pub)"
+}
+EOF
+    ) | docker \
+        run \
+        --interactive \
+        --rm \
+        --volume $(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.dot-ssh):/root/.ssh \
+        tidyrailroad/curl:0.0.0 \
+        --header "Content-Type: application/x-www-form-urlencoded" --user "${GITHUB_USER_ID}:${GITHUB_ACCESS_TOKEN}" --data @- "https://api.github.com/user/keys" &&
+    docker \
+        run \
+        --interactive \
+        --rm \
+        --entrypoint ssh-keygen \
+        --volume $(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.dot-ssh):/root/.ssh \
+        --workdir /root/.ssh \
+        tidyrailroad/openssh-client:0.0.0 \
+        -f /root/.ssh/report_id_rsa -P "${REPORT_PASSPHRASE}" -C "report" &&
+    (cat <<EOF
+{
+    "title": "report",
+    "key": "$(docker \
+        run \
+        --interactive \
+        --rm \
+        --volume $(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.dot-ssh):/root/.ssh:ro \
+        --workdir /root/.ssh \
+        alpine:3.4 \
+        cat report_id_rsa.pub)"
 }
 EOF
     ) | docker \

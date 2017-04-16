958cdf293d3747a3312bea749f5e8b2203b10d82
Emory Merryman <emory.merryman@gmail.com>
2017-04-16 11:27:59 +0000

    stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ docker \
     --volume /var/run/docker.sock:/var/run/docker.sock:ro \
     --volume $(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.entrypoint):/usr/local/src \
     --workdir /usr/local/src \
-    tidyrailroad/docker-compose:0.0.0 \
+    tidyrailroad/docker-compose:0.1.0 \
     up -d

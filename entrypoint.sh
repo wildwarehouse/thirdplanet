686bd22764b7fa5abc1369a90927fe5101bb8b2a
Emory Merryman <emory.merryman@gmail.com>
2017-04-29 12:14:58 +0000

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,8 @@ docker \
     --env DISPLAY \
     --env HOST_UID=1000 \
     --env HOST_USER=aaja0ify \
-    --env BIN=
+    --env DOT_SSH=$(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.github.dot_ssh) \
+    --env BIN=$(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.bin) \
     --env ENTRYPOINT=$(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.entrypoint) \
     --volume $(docker volume ls --quiet --filter label=com.emorymerryman.thirdplanet.structure.entrypoint):/usr/local/src \
     --workdir /usr/local/src \

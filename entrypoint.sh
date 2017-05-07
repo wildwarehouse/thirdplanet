2608faa099f1388d4bc57d04a8d7765c9f876a79
Emory Merryman <emory.merryman@gmail.com>
2017-05-07 17:56:27 +0000

    redo

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,10 @@ docker \
     --env DISPLAY \
     --env HOST_UID=1000 \
     --env HOST_USER=aaja0ify \
-    --env HOMEY=$(docker volume ls --quiet --filter label=com.emorymerryman.luckystar.structure.home) \
-    --env WORKSPACE=$(docker volume ls --quiet --filter label=com.emorymerryman.luckystar.structure.workspace) \
-    --env DOT_SSH=$(docker volume ls --quiet --filter label=com.emorymerryman.luckystar.structure.github.dot_ssh) \
     --env BIN=$(docker volume ls --quiet --filter label=com.emorymerryman.luckystar.structure.bin) \
-    --env ENTRYPOINT=$(docker volume ls --quiet --filter label=com.emorymerryman.luckystar.structure.entrypoint) \
+    --env SUDO=$(docker volume ls --quiet --filter label=com.emorymerryman.luckystar.structure.sudo) \
+    --env SBIN=$(docker volume ls --quiet --filter label=com.emorymerryman.luckystar.structure.sbin) \
+    --env WORKSPACE=$(docker volume ls --quiet --filter label=com.emorymerryman.luckystar.structure.workspace) \
     --volume $(docker volume ls --quiet --filter label=com.emorymerryman.luckystar.structure.entrypoint):/opt/entrypoint \
     --volume /var/run/docker.sock:/var/run/docker.sock:ro \
     --workdir /opt/entrypoint/luckystar \
